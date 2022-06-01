5fcd04a6732e51e75d8dee372b2e7775c9d6b946
Erick Pinglo <erick.pinglo@hotmail.com>
2022-06-01 08:32:40 -0500

    t

--- src/main/java/pe/com/bank/payment/client/CreditRestClient.java
+++ src/main/java/pe/com/bank/payment/client/CreditRestClient.java
@@ -1,6 +1,5 @@
 package pe.com.bank.payment.client;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
